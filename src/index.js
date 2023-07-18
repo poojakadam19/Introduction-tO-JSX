7fae6a367dd8f9c92bfd8a4816d2e83d7b9f4e60
poojakadam19 <60533963+poojakadam19@users.noreply.github.com>
2023-07-18 13:49:49 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Hello World! </h1>
+    <p>I am here only </p>
+  </div>,
+  document.getElementById("root")
+);
